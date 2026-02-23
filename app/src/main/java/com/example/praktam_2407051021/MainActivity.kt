09733b84cb03bcf9142c7753d1ec55fa5613f61a
mourenemillie <lifeasnanaw@gmail.com>
2026-02-23 21:39:34 +0700

    Initial commit: Setup Modul 2

---
+++ app/src/main/java/com/example/praktam_2407051021/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.praktam_2407051021
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text(
+        text = "Halo, saya Hasna dengan NPM 2407051021 siap belajar Compose! Ide project saya adalah aplikasi Daily Journaling.",
+        modifier = Modifier.padding(24.dp)
+    )
+}
